b6a90faedb7c4adbb0ea5d0d2dbf68db204408d4
Adrian Rupala <adrixop95@gmail.com>
2020-09-26 21:15:51 +0200

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(root_path="/api/v1", version="0.1.0")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"Greetings": "Hey! Thank you for using this service. If you don't know how to use it, check out readme on "
+                         "Github.",
+            "Repository": "https://github.com/Adrixop95/Barcode-Service/",
+            "MOTD": "Have a nice day : D"
+            }
